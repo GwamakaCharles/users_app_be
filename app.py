fe56e32f1ef8cc5bc32d127a3ceaaf2687eb874e
Gwamaka Mwamwaja <gmwamwaja@outlook.com>
2022-04-11 11:06:02 +0300

    used the implicit __init__ mehtod from sqlalchemy to build models

--- app.py
+++ app.py
@@ -22,12 +22,6 @@ class Users(db.Model):
     age = db.Column(db.Integer)
     country = db.Column(db.String(50))
 
-    def __init__(self, id, name, age, country):
-        self.id = id
-        self.name = name
-        self.age = age
-        self.country = country
-
     def json(self):
         return {'name': self.name, 'age':self.age, 'country':self.country}
 
