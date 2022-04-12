d603cdff3971f3ab4fc499098928d1d54923e05c
Gwamaka Mwamwaja <gmwamwaja@outlook.com>
2022-04-12 12:13:31 +0300

    added kubernetes manifests for aks

--- app.py
+++ app.py
@@ -2,15 +2,34 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_seeder import FlaskSeeder
 from flask_restful import Resource, Api
+from flask_wtf.csrf import CSRFProtect
 import redis
+import os
 from flask_caching import Cache
 
 # Creates an instance of the Flask class called 'app'
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 
+# WEBSITE_HOSTNAME exists only in production environment
+if not 'WEBSITE_HOSTNAME' in os.environ:
+   # local development, where we'll use environment variables
+   print("Loading config.development and environment variables from .env file.")
+   app.config.from_object('variables.development')
+else:
+   # production
+   print("Loading config.production.")
+   app.config.from_object('variables.production')
+
+app.config.update(
+    SQLALCHEMY_DATABASE_URI=app.config.get('DATABASE_URI'),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+)
+
+#TODO:FIX DB CONNECTION
 # Configure the sqlalchemy database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://root:postgres@database:5432/users_app_db' #The database URI that should be used for the connection.
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #This is a flag that controls whether or not modifications to SQLAlchemy-mapped objects are automatically tracked.
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://root:postgres@database:5432/users_app_db' #The database URI that should be used for the connection.
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #This is a flag that controls whether or not modifications to SQLAlchemy-mapped objects are automatically tracked.
 
 # Create an instance of the Api class for the app to create the routes
 api = Api(app)
