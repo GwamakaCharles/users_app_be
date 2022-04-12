441de381b5865661b5da18b3a169d0b1588d898a
Gwamaka Mwamwaja <gmwamwaja@outlook.com>
2022-04-12 14:04:29 +0300

    fixed imports

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_seeder import FlaskSeeder
 from flask_restful import Resource, Api
 from flask_wtf.csrf import CSRFProtect
-from resources import Users, User
 import redis
 import os
 from flask_caching import Cache
@@ -17,7 +16,6 @@ if not 'WEBSITE_HOSTNAME' in os.environ:
    # local development, where we'll use environment variables
    print("Loading config.development and environment variables from .env file.")
    app.config.from_object('env_variables.development')
-#    app.config.from_object('env_variables.development')
 else:
    # production
    print("Loading config.production.")
@@ -34,9 +32,39 @@ api = Api(app)
 # Initialize the database connection
 db = SQLAlchemy(app)
 
-
 # app.config.from_object('env_variables.development')
 cache = Cache(app)
+
+class Users(Resource):
+    @cache.cached(timeout=30)
+    def get(self):
+        return {'users' : [user.json() for user in UserModel.query.paginate().items]}
+
+class User(Resource):
+    @cache.cached(timeout=30)
+    def get(self,name):
+        user = UserModel.find_user_by_name(name)
+        return user.json()
+
+
+class UserModel(db.Model):
+    """
+    Create a new user model with the given name, age and country
+    """
+    __tablename__ = 'users' # table name
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    age = db.Column(db.Integer)
+    country = db.Column(db.String(50))
+
+    def json(self):
+        return {'name': self.name, 'age':self.age, 'country':self.country}
+    
+    @classmethod
+    @cache.cached(timeout=30)
+    def find_user_by_name(cls,name): 
+       return cls.query.filter_by(name=name).first_or_404()
+
 
 @app.before_first_request
 def create_tables():
@@ -46,7 +74,6 @@ def create_tables():
 @app.route('/', methods=['GET'])
 def home():
     return f"The endpoints available are /users and /user/<name>. For example, /users will return all the users and /user/<name> will return the user with the given name."
-
 
 # endpoint to retrieve all users
 api.add_resource(Users, '/users')
